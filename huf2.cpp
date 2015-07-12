168ab438c0402ee1218240afbcbd8821ed960cb0
Jackson Lucas <jackson7br@gmail.com>
2015-07-12 14:23:37 -0400

    failing in generate code

--- huf2.cpp
+++ huf2.cpp
@@ -12,31 +12,64 @@
 
 using namespace std;
 
+class Column  {
+  public:
+    string code;
+    char symbol;
+    Column(char symbol, string code);
+};
+
+Column::Column(char symbol, string code) {
+  this->symbol = symbol;
+  this->code = code;
+}
 
 class Node {
   public:
     int frequency;
+    bool hasSymbol;
     char symbol;
     vector<Node> adjacents;
+    vector<Column> codes;
     Node(int frequency, char symbol);
     Node(int frequency, vector<Node> adjacents);
     void print();
     void printOne();
     void plusOneFrequency();
+    void generateCode(vector<Column> codes, string code);
+    void printCodes();
 };
+
+void Node::generateCode(vector<Column> codes, string code = "") {
+
+  if(this->hasSymbol) {
+    codes.push_back(Column (this->symbol, code));
+  }
+
+  if(adjacents.size() > 0) {
+    adjacents[0].generateCode(codes, code + '1');
+  }
+
+  if(adjacents.size() > 1) {
+    adjacents[1].generateCode(codes, code + '0');
+  }
+}
 
 Node::Node(int frequency, char symbol) {
   this->frequency = frequency;
+  this->hasSymbol = true;
   this->symbol = symbol;
 }
 
 Node::Node(int frequency, vector<Node> adjacents) {
   this->frequency = frequency;
+  this->hasSymbol = false;
   this->adjacents = adjacents;
 }
 
 void Node::print() {
   cout << " ( ";
+  // the lesser to the left
   if(adjacents.size() > 0) {
     adjacents[0].print();
   }
@@ -51,6 +84,13 @@ void Node::print() {
 
 void Node::printOne() {
   cout << symbol << ": " << frequency << endl;
+}
+
+void Node::printCodes() {
+  cout << "Printing Codes" << endl;
+  for(int index=0; index<codes.size(); index++) {
+    cout << codes[index].symbol << ": " << codes[index].code << endl;
+  }
 }
 
 void Node::plusOneFrequency() {
@@ -98,13 +138,20 @@ Node Graph::getTree() {
     vector<Node> adjacents;
 
     index = getLesserFrequency();
-    adjacents.push_back(nodes[index]);
+    Node nodeLesser1 = nodes[index];
     nodes.erase(nodes.begin()+index);
 
     index = getLesserFrequency();
-    adjacents.push_back(nodes[index]);
+    Node nodeLesser2 = nodes[index];
     nodes.erase(nodes.begin()+index);
 
+    if(!nodeLesser2.hasSymbol && nodeLesser1.hasSymbol) {
+      adjacents.push_back(nodeLesser2);
+      adjacents.push_back(nodeLesser1);
+    } else {
+      adjacents.push_back(nodeLesser1);
+      adjacents.push_back(nodeLesser2);
+    }
 
     Node newNode(adjacents[0].frequency + adjacents[1].frequency, adjacents);
     //newNode.printOne();
@@ -113,12 +160,6 @@ Node Graph::getTree() {
 
   return nodes[0];
 }
-
-class Table {
-  public:
-    string code;
-    char symbol;
-};
 
 int getIndexBySymbol(char symbol, vector<Node> graph) {
   for(int index=0; index<graph.size(); index++) {
@@ -158,19 +199,23 @@ void printVector(vector<char> vertex) {
   cout << endl;
 }
 
+// adjacent[0] means 1 in code
+// adjacent[1] means 0 in code
 int main(int argc, char** argv) {
   std::ifstream infile(argv[1], std::ios::binary);
 
   std::vector<char> data = std::vector<char>
     (std::istreambuf_iterator<char>(infile), std::istreambuf_iterator<char>());
 
-  printVector(data);
+  //printVector(data);
 
   Graph graph = getGraph(data);
-  //graph.printNodes();
+  graph.printNodes();
   Node root = graph.getTree();
   root.print();
-
+  cout << endl;
+  root.generateCode(root.codes);
+  root.printCodes();
   infile.close();
 
   return 0;
